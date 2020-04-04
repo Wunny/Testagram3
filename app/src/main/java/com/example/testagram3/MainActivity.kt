813637531d0e26300be547a2eb82a4e3d4d7d55a
HOME <dndnjs511@naver.com>
2020-04-04 20:46:21 +0900

    navigation bar

--- app/src/main/java/com/example/testagram3/MainActivity.kt
+++ app/src/main/java/com/example/testagram3/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.testagram3
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.testagram3.navigation.AlarmFragment
-import com.example.testagram3.navigation.DetailviewFragment
-import com.example.testagram3.navigation.GridFragment
-import com.example.testagram3.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.testagram3.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
@@ -31,7 +34,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo ->{
-
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm ->{
